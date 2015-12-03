e0bab21d0799e71e0c9cb7a5cebb22e56b4b1eeb
stuarttiedemann <stuarttiedemann@yahoo.com>
2015-12-03 15:40:35 -0500

    Simple Chat program using node js

---
+++ main.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(request, response){
+	fs.readFile('client.html', 'utf-8', function (error,data){
+		response.writeHead(200,{'Content-type':'text/html'});
+		response.write(data);
+		response.end();
+	})
+})
+server.listen(21000);
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection',function(socket){
+	socket.on('message_to_server',function(data){
+		io.sockets.emit('message_to_client',{
+			message:data["message"]
+		})
+	})
+})
+console.log("Listening to port 21000");
